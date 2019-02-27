22e67fc3986ed38879e773ec59b4fcb5fc0823a8
Mattias Nording <mattiasnording@gmail.com>
2019-02-27 06:15:25 +0100

    intial support for mediastretegi

--- woo-addon-klarna-shippingtocapture.php
+++ woo-addon-klarna-shippingtocapture.php
@@ -11,32 +11,44 @@ Domain Path: /languages
 */
 require 'vendor/autoload.php';
 class KlarnaWooAddShippingToCapture{
+    private $eutestid;
+        private $eutestpass;
+       private $euliveid;
+        private $eulivepass;
     function __construct()
     {
+        $gateway_settings = get_option('woocommerce_kco_settings');
+        $this->eutestid = $gateway_settings["test_merchant_id_eu"];
+        $this->eutestpass = $gateway_settings["test_shared_secret_eu"];
+        $this->euliveid = $gateway_settings["merchant_id_eu"];
+        $this->eulivepass = $gateway_settings["shared_secret_eu"];
         add_action('woocommerce_order_status_completed', array($this, 'addShippingToCapture'), 999999);
+        
     }
    function addShippingToCapture($order_id){
+        
     $order = wc_get_order($order_id);
-    $order->add_order_note( "trying to add shipment To klarna order" );
-    $order->save();
     $klanrnaorderid= $order->get_transaction_id();
         $captuereid = get_post_meta( $order_id, '_wc_klarna_capture_id', true );
-        $trackingId = get_post_meta($order_id,'trackingIdForORder',true);
+        $trackingId = get_post_meta($order_id,'_msunifaun_web-ta_order_consignment_id',true);
         var_dump($captuereid);
         var_dump($trackingId);
+        
         $client = new \GuzzleHttp\Client();
-        $res = $client->post( "https://api.playground.klarna.com/ordermanagement/v1/orders/".$klanrnaorderid."/captures/".$captuereid."/shipping-info", ['verify' => true, 'auth' => ["PK04149_9ef50d19b0e3", "S3Pl4Di5ovDw0711"], 'json' => [
+        $res = $client->post( "https://api.playground.klarna.com/ordermanagement/v1/orders/".$klanrnaorderid."/captures/".$captuereid."/shipping-info", ['verify' => true, 'auth' => [$this->eutestid, $this->eutestpass], 'json' => [
                 "shipping_info"=> [
                     [
-                        "shipping_company"=> "DHL",
+                        "shipping_company"=> "DHL Freight",
                         "shipping_method"=> "PickUpPoint",
-                        "tracking_number"=> "63456415674545679874",
-                        "tracking_uri"=> "http://shipping.example/findmypackage?63456415674545679874"
+                        "tracking_number"=> $trackingId,
+                        "tracking_uri"=> "https://droppbollen.se/spara-paket/?orderid=".$order_id
                     ]
                 ]
         ], 'headers' => [
             'User-Agent' => 'Mnording Addon Shipping WP-Plugin',
         ]]);
+        $order->add_order_note( "Updated Klarna whith shipment data. Gave them tracking ID ".$trackingId );
+        $order->save();
    }    
 }
 $t = new KlarnaWooAddShippingToCapture();
